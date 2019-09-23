d35ee4e6eab04dd8d29b68269aa42ea4df6668a2
Ubuntu <ubuntu@ip-172-31-31-42.ec2.internal>
2019-09-23 00:38:59 +0000

    Initial commit

---
+++ more_length_width.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Joseph Palermo
+# Created on: September 2019
+# My multiple test cases
+
+print("Enter length of rectangle (mm): 6")
+print("Enter perimeter of rectangle (mm): 6")
+
+
+def main():
+    print("Area is {}" .format(6*6))  # a=lw The Area Equation
+    print("Perimeter is {}" .format(2*(6+6)))  # 2(l+w) The Perimeter Equation
+
+
+if __name__ == "__main__":
+    main()
